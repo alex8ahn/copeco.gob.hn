58e1dcc0073b072c51eff7b3ec8aedec6d27aff4
alex8ahn <alex8a1991@gmail.com>
2022-08-08 09:34:41 -0600

    modificaciones en enviroment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { VisionComponent } from './pages/vision/vision.component';
+import { RegionalesComponent } from './pages/regionales/regionales.component';
 
 
 const routes: Routes = [
 {path: '', component: HomeComponent},
 {path: 'vision', component: VisionComponent},
+{path: 'regionales', component: RegionalesComponent},
 
 ];
 
